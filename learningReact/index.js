0184a60e8bf68d80e9bc5c2db11abf3e1b078a6f
zamimi <pacanut.zamanthashane@gmail.com>
2025-11-20 22:34:16 +0800

    My folder for all things React related

---
+++ learningReact/index.js
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import Prac from "./index";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Prac />
+  </StrictMode>
+);
---
+++ learningReact/react.js
@@ -0,0 +1,14 @@
+function MyButton() {
+    return (
+        <button> I'm a button </button>
+    );
+}
+
+export default function MyApp () {
+    return (
+        <div>
+            <h1> Hello, welcome to my React learning page! </h1>
+            <MyButton />
+        </div>
+    );
+}
